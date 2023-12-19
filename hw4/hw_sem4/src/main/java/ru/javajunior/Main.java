d5215b44760a60c3a711caae2c8caea37fc4442e
Vladislav <vlad-pugovkin2@tandex.ru>
2023-12-19 18:59:36 +0300

    Add HW4ДОобавил 4 домашнюю работу, рÐаботает пока только из мейна, докер-компоуз в процессе

---
+++ hw4/hw_sem4/src/main/java/ru/javajunior/Main.java
@@ -0,0 +1,16 @@
+package ru.javajunior;
+
+public class Main {
+    /*
+        Создайте базу данных (например, SchoolDB).
+        В этой базе данных создайте таблицу Courses с полями id (ключ), title, и duration.
+        Настройте Hibernate для работы с вашей базой данных.
+        Создайте Java-класс Course, соответствующий таблице Courses, с необходимыми аннотациями Hibernate.
+        Используя Hibernate, напишите код для вставки, чтения, обновления и удаления данных в таблице Courses.
+        Убедитесь, что каждая операция выполняется в отдельной транзакции.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ hw4/hw_sem4/src/main/java/ru/javajunior/models/Course.java
@@ -0,0 +1,2 @@
+package ru.javajunior.models;public class Course {
+}
