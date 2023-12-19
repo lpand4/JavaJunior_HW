b16da4166e2152bd0d8ac6d6aef70a74ad8448e8
Vladislav <vlad-pugovkin2@tandex.ru>
2023-12-19 19:38:55 +0300

    Add HW4 without Docker

--- hw4/hw_sem4/src/main/java/ru/javajunior/Main.java
+++ hw4/hw_sem4/src/main/java/ru/javajunior/Main.java
@@ -1,5 +1,18 @@
 package ru.javajunior;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.javajunior.models.Course;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
+
+
 public class Main {
     /*
         Создайте базу данных (например, SchoolDB).
@@ -9,8 +22,108 @@ public class Main {
         Используя Hibernate, напишите код для вставки, чтения, обновления и удаления данных в таблице Courses.
         Убедитесь, что каждая операция выполняется в отдельной транзакции.
      */
+    static Random rnd = new Random();
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory()) {
+
+            //Вставка в таблицу
+            for (int i = 0; i < rnd.nextInt(15, 31); i++) {
+                Course course = Course.create();
+                // addCourse(sessionFactory,course);
+            }
+            //Чтение всех курсов
+            List<Course> courseList = readAllCourses(sessionFactory);
+            courseList.forEach(System.out::println);
+
+            //Получение курса по id
+            Course courseWithId = readCourseById(sessionFactory, 13);
+            System.out.println(courseWithId);
+
+            //Обновление данных
+            updateCourse(sessionFactory, 15);
+
+            //Удаление по ID
+            deleteCourseById(sessionFactory, 17);
+
+            //Удаление всех курсов
+            //deleteAllCourses(sessionFactory);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private static void addCourse(SessionFactory sessionFactory, Course course) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.save(course);
+            session.getTransaction().commit();
+            System.out.println("Добавление курса прошло успешно!");
+        }
+    }
+
+    private static List<Course> readAllCourses(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            List<Course> courseList = session.createQuery("FROM Course").getResultList();
+            System.out.println("Чтение курсов прошло успешно!");
+            return courseList;
+        }
+    }
+
+    private static Course readCourseById(SessionFactory sessionFactory, int id) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Course course = session.get(Course.class, id);
+            if (course != null)
+                System.out.println("Чтение курса прошло успешно!");
+            else
+                System.out.println("Курса с таким ID нет!");
+            return course;
+        }
+    }
+
+    private static void updateCourse(SessionFactory sessionFactory, int id) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Course course = session.get(Course.class, id);
+            if (course != null) {
+                System.out.println("Ныняшние данные этого курса: " + course);
+                course.randomUpdate();
+                System.out.println("Новые данные этого курса: " + course);
+                session.update(course);
+                session.getTransaction().commit();
+            } else
+                System.out.println("Курса с таким ID нет!");
+        }
+    }
+
+    private static void deleteCourseById(SessionFactory sessionFactory, int id) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Course courseToDelete = session.get(Course.class, id);
+            if (courseToDelete != null) {
+                session.delete(courseToDelete);
+                session.getTransaction().commit();
+                System.out.println("Курс успешно удален!");
+            } else
+                System.out.println("Курса с таким ID нет!");
+
+        }
+    }
+
+    private static void deleteAllCourses(SessionFactory sessionFactory) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            List<Course> courseList = session.createQuery("FROM Course", Course.class).getResultList();
+            courseList.forEach(session::delete);
+            session.getTransaction().commit();
+        }
+    }
+
+
 }
--- hw4/hw_sem4/src/main/java/ru/javajunior/models/Course.java
+++ hw4/hw_sem4/src/main/java/ru/javajunior/models/Course.java
@@ -1,2 +1,80 @@
-package ru.javajunior.models;public class Course {
+package ru.javajunior.models;
+
+import javax.persistence.*;
+import java.util.Random;
+
+@Entity
+@Table(name = "Courses")
+public class Course {
+
+    private static final String[] titles = new String[] { "Математика", "Алгебра", "Геометрия", "Вероятность и статистика",
+            "Информатика", "Литература", "География", "Биология", "Физика", "Химия",
+            "ОБЖ", "История", "Обществознание", "Экономика", "Право", "Русский язык"};
+    private static final Random rnd = new Random();
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "duration")
+    private int duration;
+
+
+    public Course(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public Course() {
+    }
+
+    public static Course create(){
+        return new Course(titles[rnd.nextInt(titles.length)], rnd.nextInt(5, 12));
+    }
+    public void randomUpdate(){
+        String updatedTitle;
+        int updatedDuration;
+        do {
+            updatedTitle = titles[rnd.nextInt(titles.length)];
+        }while (updatedTitle.equals(getTitle()));
+        setTitle(updatedTitle);
+        do {
+            updatedDuration = rnd.nextInt(5, 12);
+        }while (updatedDuration == getDuration());
+        setDuration(rnd.nextInt(5, 12));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
 }
